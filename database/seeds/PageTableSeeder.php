fd2d2a469a803bcb7be8fd7f808c99c9ca135d0e
sunzheng19911227 <452017791@qq.com>
2016-01-12 17:59:16 +0800

    laravel

---
+++ database/seeds/PageTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Page;
+
+class PageTbaleSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+        DB::table('pages')->delete();
+
+        for ($i = 0; $i< 10; $i++) {
+            Page::create([
+                'title' => 'Title '.$i,
+                'slug'  => 'first-page',
+                'body'  => 'Body '.$i,
+                'user_id' => 1,
+            ]);
+        }
+	}
+
+}
